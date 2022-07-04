dd3047b6902a8b1f788fc119641372c86394015e
guo liang <liang.guo@purspec.cn>
2022-07-04 15:12:04 +0800

    update project settings: TIME_ZONE = "Asia/Shanghai" LANGUAGE_CODE = "zh-hans"

--- config/settings/base.py
+++ config/settings/base.py
@@ -23,9 +23,11 @@ DEBUG = env.bool("DJANGO_DEBUG", False)
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
 # In Windows, this must be set to your system time zone.
-TIME_ZONE = "UTC"
+# TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Shanghai"
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
-LANGUAGE_CODE = "en-us"
+# LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "zh-hans"
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
 SITE_ID = 1
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
